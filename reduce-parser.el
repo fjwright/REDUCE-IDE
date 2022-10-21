f281a237898aba7ea9681d9ffaf050f3544496cf
Francis J. Wright <fjw@monolith>
2022-10-21 15:48:36 +0100

    Parser -- use intervals
    Use a list of intervals containing comment statements instead of a
    list of start and end positions, which halves the length of the list.

--- reduce-parser.el
+++ reduce-parser.el
@@ -1,56 +1,52 @@
 ;; An analogue of syntax-ppss for REDUCE comment statements.
 
-(defvar-local reduce--comment-alist nil
-  "Alist of elements of the form (position . in).
-The car is the position at the start or immediately after the end
-of a comment statement, and the cdr is t at the start and nil at
-the end.")
+(defvar-local reduce--comment-seq nil
+  "List of elements of the form (start . finish).
+The car of each element is the position at the start of a comment
+statement and the cdr is the position immediately after the end
+of that comment statement.")
 
-(defun reduce--update-comment-alist ()
+(defun reduce--build-comment-seq ()
   "Scan the buffer for comment statements.
-Use the information found to update ‘reduce--comment-alist’."
-  (let ((case-fold-search t) alist)
+Use the information found to build ‘reduce--comment-seq’."
+  (let ((case-fold-search t) lst)
     (save-excursion
       (save-restriction
         (widen) (goto-char (point-min))
         (while (re-search-forward "\\_<comment\\_>" nil t)
           ;; Unless in a string or syntactic comment...
           (unless (nth 8 (syntax-ppss))
-            (push (cons (match-beginning 0) t) alist)
-            (when (re-search-forward "[\;$]" nil t)
-              (push (cons (match-end 0) nil) alist))))))
-    (setq reduce--comment-alist (nreverse alist))))
+            (push (cons (match-beginning 0)
+                        (or (re-search-forward "[\;$]" nil t) (point-max)))
+                  lst)))))
+    (setq reduce--comment-seq (nreverse lst))))
 
 (defun reduce--highlight-comment-statements ()
   "Highlight all comment statements in the buffer.
-Only works reliably with font-lock off, so turn font-lock off.
+Only works reliably with font-lock off, so turns font-lock off.
 Purely intended for testing."
   (font-lock-mode 0)
   (with-silent-modifications
-    (let ((alist (or reduce--comment-alist
-                     (reduce--update-comment-alist))))
-      (while alist
-        (let ((start (caar alist))
-              (end (or (caadr alist) (point-max))))
-          (setq alist (cddr alist))
-          (add-face-text-property start end 'highlight))))))
+    (let ((lst (or reduce--comment-seq
+                   (reduce--build-comment-seq))))
+      (while lst
+        (add-face-text-property (caar lst) (cdar lst) 'highlight)
+        (setq lst (cdr lst))))))
 
 (defun reduce--in-comment-statement-p (&optional pos)
-  "Return non-nil if POS is within a comment statement.
+  "Return t if POS is within a comment statement; nil otherwise.
 If POS is omitted then it defaults to point."
   (unless pos (setq pos (point)))
-  (let ((alist (or reduce--comment-alist
-                   (reduce--update-comment-alist)))
+  (let ((lst (or reduce--comment-seq
+                 (reduce--build-comment-seq)))
         value)
-    (while alist
-      (let ((start (caar alist))
-            (end (or (caadr alist) (point-max))))
+    (while lst
+      (let ((start (caar lst)) (end (cdar lst)))
         (cond ((< end pos)              ; comment before pos
-               (setq alist (cddr alist)))
-              ((eq end pos)             ; comment ends at pos
-               (setq alist nil value nil))
-              ((< pos start)            ; pos before comment
-               (setq alist nil value nil))
+               (setq lst (cdr lst)))
+              ((or (eq end pos)         ; comment ends at pos
+                   (< pos start))       ; pos before comment
+               (setq lst nil))
               ;; start <= pos < end -- within comment
-              (t (setq alist nil value t)))))
+              (t (setq lst nil value t)))))
     value))
