66ff431b6a3034a8e018b31098c094dd90816670
Francis J. Wright <fjw@monolith>
2022-10-22 17:53:39 +0100

    Parser -- use vector
    Make reduce--comment-seq a vector or nil if there are no comment
    statements, which is an important special case.  Use mapc instead of
    seq-do.  Revise the bisection algorithm, which now seems to work and
    examines uniformly around log2 n of the n comment statement intervals,
    rather than the average n/2 for a linear search.

--- reduce-parser.el
+++ reduce-parser.el
@@ -1,12 +1,11 @@
 ;; An analogue of syntax-ppss for REDUCE comment statements.
 
-(require 'seq)
-
 (defvar-local reduce--comment-seq nil
-  "List of elements of the form (start . finish).
+  "A sequence of elements of the form (start . finish), or nil.
 The car of each element is the position at the start of a comment
 statement and the cdr is the position immediately after the end
-of that comment statement.")
+of that comment statement.  The value is nil if there are no
+comment statements, which is a common special case.")
 
 (defun reduce--build-comment-seq ()
   "Scan the buffer for comment statements.
@@ -21,41 +20,44 @@ Use the information found to build ‘reduce--comment-seq’."
             (push (cons (match-beginning 0)
                         (or (re-search-forward "[\;$]" nil t) (point-max)))
                   lst)))))
-    (setq reduce--comment-seq (nreverse lst))))
+    (setq reduce--comment-seq (and lst (nreverse (vconcat lst))))))
 
 (defun reduce--highlight-comment-statements ()
   "Highlight all comment statements in the buffer.
 Only works reliably with font-lock off, so turns font-lock off.
 Purely intended for testing."
-  (font-lock-mode 0)
-  (with-silent-modifications
-    (seq-do
-     (lambda (i)                        ; i = (start . finish)
-       (add-face-text-property (car i) (cdr i) 'highlight))
-     (or reduce--comment-seq
-         (reduce--build-comment-seq)))))
+  (let ((seq (or reduce--comment-seq (reduce--build-comment-seq))))
+    (when seq                           ; nil if no comment statements
+      (font-lock-mode 0)
+      (with-silent-modifications
+        (mapc
+         (lambda (i)                    ; i = (start . finish)
+           (add-face-text-property (car i) (cdr i) 'highlight))
+         seq)))))
 
 (defun reduce--in-comment-statement-p (&optional pos)
   "Return t if POS is within a comment statement; nil otherwise.
 If POS is omitted then it defaults to point."
-  (unless pos (setq pos (point)))
-  (let* ((seq (or reduce--comment-seq
-                  (reduce--build-comment-seq)))
-         (lower 0) (upper (1- (length seq))) value)
-    ;; seq is a sequence of integer intervals.  Use bisection to
-    ;; search seq for an interval surrounding POS.  lower < mid <
-    ;; upper are indices into seq and mid is the (integer) mid-point
-    ;; between lower and upper.  ivl is the mid-point interval.
-    (while (< lower upper)
-      (let* ((mid (/ (+ lower upper) 2)) (ivl (elt seq mid))
-             (start (car ivl)) (finish (cdr ivl)))
-        (cond ((< finish pos)           ; interval < pos
-               (setq lower mid))
-              ((< pos start)            ; pos < interval
-               (setq upper mid))
-              ((eq finish pos)          ; pos = interval-finish
-               (setq lower upper))      ; -- stop loop
-              ;; interval-start <= pos < interval-finish
-              ;; so pos within interval -- stop loop
-              (t (setq lower upper value t)))))
-    value))
+  (let ((seq (or reduce--comment-seq (reduce--build-comment-seq))))
+    (when seq                           ; nil if no comment statements
+      (unless pos (setq pos (point)))
+      (let ((lower 0) (upper (1- (length seq))) value (count 0))
+        ;; seq is a sequence of integer intervals.  Use bisection to
+        ;; search seq for an interval surrounding POS.  lower < mid <
+        ;; upper are indices into seq and mid is the (integer) mid-point
+        ;; between lower and upper.  ivl is the mid-point interval.
+        (while (<= lower upper)
+          ;; (setq count (1+ count))
+          (let* ((mid (/ (+ lower upper) 2)) (ivl (elt seq mid))
+                 (start (car ivl)) (finish (cdr ivl)))
+            (cond ((< finish pos)       ; interval < pos
+                   (setq lower (1+ mid)))
+                  ((< pos start)        ; pos < interval
+                   (setq upper (1- mid)))
+                  ((eq finish pos)      ; pos = interval-finish
+                   (setq lower (1+ upper))) ; -- stop loop
+                  ;; interval-start <= pos < interval-finish
+                  ;; so pos within interval -- stop loop
+                  (t (setq lower (1+ upper) value t)))))
+        ;; (message "intervals: %s; searches: %s" (length seq) count)
+        value))))
