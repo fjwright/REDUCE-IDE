7d5133cddadc9019a87946ff1cabe8d54836376d
Francis Wright <59773998+fjwright@users.noreply.github.com>
2025-03-16 17:52:26 +0000

    Fix REDUCE Run mode
    Revert to old keymap functions in code handling the Fontification submenu.

--- reduce-font-lock.el
+++ reduce-font-lock.el
@@ -4,7 +4,7 @@
 
 ;; Author: Francis J. Wright <https://sites.google.com/site/fjwcentaur>
 ;; Created: 6 June 2022 as a separate file (was part of reduce-mode.el)
-;; Time-stamp: <2025-03-15 15:17:34 franc>
+;; Time-stamp: <2025-03-16 17:44:21 franc>
 ;; Homepage: https://reduce-algebra.sourceforge.io/reduce-ide/
 
 ;; This file is part of REDUCE IDE.
@@ -580,9 +580,12 @@ their names should not be taken too literally!")
   (add-to-list 'font-lock-extend-region-functions
                #'reduce-font-lock--extend-region-for-comment-statement)
   (reduce-font-lock--level)             ; for font-lock menu
-  (keymap-set-after
-    (keymap-lookup reduce-run-mode-map "<menu-bar> <Run\ REDUCE>")
-    "<Fontification>" (cons "Syntax Highlighting" reduce-fontification-submenu)
+  ;; The following code using new keymap functions fails:
+  ;; (keymap-set-after
+  ;;   (keymap-lookup reduce-run-mode-map "<menu-bar> <Run\ REDUCE>")
+  ;;   "<Fontification>" (cons "Syntax Highlighting" reduce-fontification-submenu)
+  (define-key-after (lookup-key reduce-run-mode-map [menu-bar Run\ REDUCE])
+    [Fontification] (cons "Syntax Highlighting" reduce-fontification-submenu)
     t))
 
 (defconst reduce-font-lock--run-keywords-0
